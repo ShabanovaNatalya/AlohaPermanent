852196a85aaaafa66d6a0f0846dea9a0fcd1c680
ShabanovaNatalya <natalikurilova@icloud.com>
2025-05-22 22:17:34 +0400

    tailwindcss изменения

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./public/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  theme: {
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 4px)',
+				sm: 'calc(var(--radius) - 12px)',
+				xs: 'calc(var(--radius) - 16px)',
+				xss: 'calc(var(--radius) - 20px)',
+			},
+			colors: {
+				background: 'hsl(var(--background))',
+
+				common: {
+					black: 'hsl(var(--common-black))',
+					white: 'hsl(var(--common-white))',
+				},
+				primaryText: 'hsl(var(--grey-800))', 
+				secondaryText: 'hsl(var(--grey-600))',
+			},
+			fontFamily: {
+				sans: ['NunitoSans', ...defaultTheme.fontFamily.sans],
+				manrope: ['Forum', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
 }
